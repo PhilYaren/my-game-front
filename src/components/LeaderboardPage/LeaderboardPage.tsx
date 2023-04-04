896fceea66ffc2fe92facee02241a42dbfd63f84
Pavel <goncharov_p@bk.ru>
2023-04-04 14:21:34 +0300

    leaderboards and statistics

--- src/components/LeaderboardPage/LeaderboardPage.tsx
+++ src/components/LeaderboardPage/LeaderboardPage.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleLeaderboardComponent from "../SingleGameLeaderboardComponent/SingleLeaderboardComponent";
 
 export default function LeaderboardPage(): JSX.Element {
-    const leaderboardArr = []
-  return (
+  const [leaderboardArr, setLeaderboard] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/api/statistics", {
+          method: "GET",
+          credentials: "include",
+        });
+        const leaderboard = await response.json();
+        setLeaderboard(leaderboard);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+}, []);
+
+return (
     <div>
       <h2>LeaderboardPage</h2>
       {leaderboardArr.map((singleGameset) => (
-        <SingleLeaderboardComponent gameset={singleGameset} />
+        <SingleLeaderboardComponent
+          key={singleGameset.createdAt}
+          gameset={singleGameset}
+        />
       ))}
     </div>
   );
--- src/components/SingleGameLeaderboardComponent/SingleLeaderboardComponent.tsx
+++ src/components/SingleGameLeaderboardComponent/SingleLeaderboardComponent.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import SingleLeaderboardUserRow from "../SingleLeaderboardUserRow/SingleLeaderboardUserRow";
 
 export default function SingleLeaderboardComponent({gameset}): JSX.Element {
   return (
     <div>
-      <d4>SingleLeaderboardComponent</d4>
+      <h4>{gameset.name}</h4>
       <ol>
-        
+        {gameset.statistics.map((singleStat) => (
+          <SingleLeaderboardUserRow key={singleStat.id} userName={singleStat.user.userName} score={singleStat.score} time={singleStat.createdAt
+          } />
+        ))}
       </ol>
     </div>
   );
