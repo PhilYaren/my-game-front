aed5641d0792325ede8b7c579a43f0f835197649
Pavel <goncharov_p@bk.ru>
2023-04-04 12:26:39 +0300

    halfDone leaderboards

---
+++ src/components/SingleGameLeaderboardComponent/SingleLeaderboardComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SingleLeaderboardComponent({gameset}): JSX.Element {
+  return (
+    <div>
+      <d4>SingleLeaderboardComponent</d4>
+      <ol>
+        
+      </ol>
+    </div>
+  );
+}
